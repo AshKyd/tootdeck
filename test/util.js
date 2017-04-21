9d32b3661db701266e1e0680182ad6229acb8d41
Ash Kyd <ash@kyd.com.au>
2017-04-21 20:19:12 +1000

    various util tests

--- test/util.js
+++ test/util.js
@@ -35,3 +35,47 @@ describe('acctToMention', () => {
     );
   });
 });
+describe('getReplyContent', () => {
+  function makeStatus(username, mentions){
+    const status = {
+      id: 1,
+      account: {
+        acct: username,
+      },
+    };
+    if(mentions){
+      status.mentions = mentions.map(mention => ({
+        acct: mention,
+      }))
+    }
+    return status;
+  }
+  it('should return one username when no mentions are set', () => {
+    const status = makeStatus('AshKyd');
+    assert.deepEqual(
+      util.getReplyContent(status),
+      '@AshKyd'
+    );
+  });
+  it('should return one username when the poster is replying to themselves', () => {
+    const status = makeStatus('AshKyd', ['AshKyd']);
+    assert.deepEqual(
+      util.getReplyContent(status),
+      '@AshKyd'
+    );
+  });
+  it('should return two usernames when the user is replying to someone', () => {
+    const status = makeStatus('AshKyd', ['MaloryArcher']);
+    assert.deepEqual(
+      util.getReplyContent(status),
+      '@AshKyd @MaloryArcher'
+    );
+  });
+  it('should return any number of usernames', () => {
+    const status = makeStatus('AshKyd', ['MaloryArcher', 'LenTrexler']);
+    assert.deepEqual(
+      util.getReplyContent(status),
+      '@AshKyd @MaloryArcher @LenTrexler'
+    );
+  });
+});
